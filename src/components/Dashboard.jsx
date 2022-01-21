61f9a64c9cfe141c71c1ff15b18594d5613bd93a
Sma-source <rabhi.smain@gmail.com>
2022-01-21 23:30:50 +0100

    add tailwind & create component

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -21,7 +21,7 @@ const Dashboard = () => {
 
   return (
     <div className={styles.container}>
-      <aside className={cx(styles.col, styles.sidebar)}>
+      {/* <aside className={cx(styles.col, styles.sidebar)}>
         <h1 className="text-3xl font-bold underline"> Hello world! </h1>
 
         <br />
@@ -48,7 +48,57 @@ const Dashboard = () => {
             <button className={styles.btnDownload}>Télécharger</button>
           </div>
         </div>
-      </section>
+      </section> */}
+
+      <div className="relative bg-black">
+        <div className="flex flex-col sm:flex-row sm:justify-around">
+          <div className="w-64 h-screen">
+            <div className="mt-10 px-6">
+              {error && <Alert props={error} />}
+              <p className="mx-2 p-2 my-6 text-white font-poppins text-sm font-semibold">
+                {user && user.email}
+              </p>
+            </div>
+            <nav className="mt-10 px-6 ">
+              <Link
+                className="hover:text-purple  flex items-center p-2 my-6 transition-colors duration-200  text-white "
+                to="#"
+              >
+                <span className="mx-2 text-lg font-poppins font-semibold uppercase">
+                  Mes QR Codes
+                </span>
+                <span className="flex-grow text-right"></span>
+              </Link>
+              <Link
+                className="hover:text-purple  flex items-center p-2 my-6 transition-colors duration-200  text-white "
+                to="#"
+              >
+                <span className="mx-2 text-lg font-poppins font-semibold uppercase">
+                  Créer un qr code
+                </span>
+                <span className="flex-grow text-right"></span>
+              </Link>
+              <Link
+                className="hover:text-purple  flex items-center p-2 my-6 transition-colors duration-200  text-white"
+                to="/update-profile"
+              >
+                <span className="mx-2 text-lg font-poppins font-semibold uppercase">
+                  Mon Compte
+                </span>
+                <span className="flex-grow text-right"></span>
+              </Link>
+            </nav>
+            <div className="mt-10 px-6">
+              <button
+                onClick={handleLogout}
+                className={`${styles.btnDeconnect} mx-2 p-2 my-6 py-24 sm:py-52  `}
+              >
+                Déconnexion
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,21 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        black: "#101010",
+        white: "#ffffff",
+        purple: "#6e86e2",
+        midnight: "#121063",
+        metal: "#565584",
+        tahiti: "#3ab7bf",
+        silver: "#ecebff",
+        "bubble-gum": "#ff77e9",
+        bermuda: "#78dcca",
+      },
+    },
   },
   plugins: [],
 };
