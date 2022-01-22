92b157df6362a93e4af161dbbe51d26f9d4e8458
Sma-source <rabhi.smain@gmail.com>
2022-01-22 23:55:08 +0100

    create: details component

---
+++ src/components/Details.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LayoutDash from "./LayoutDash";
+
+function Details() {
+  return (
+    <LayoutDash>
+      <h1>Details</h1>
+    </LayoutDash>
+  );
+}
+
+export default Details;
