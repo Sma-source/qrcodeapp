8c07ae8cadf8821bce75000091102008f611e028
Sma-source <rabhi.smain@gmail.com>
2022-01-22 23:54:19 +0100

    modify: absolute path

--- src/components/SidebarData.js
+++ src/components/SidebarData.js
@@ -1,19 +1,19 @@
 export const SidebarData = [
   {
     title: "Mes QR Codes",
-    path: "mes-qr-codes",
+    path: "/",
     cName:
       "block text-white font-Poppins font-semibold hover:text-purple text-sm md:text-[20px]  mb-[40px] md:mx-4",
   },
   {
     title: "Créer un QR Code",
-    path: "creer-qr-code",
+    path: "/creer-qr-code",
     cName:
       " block text-white font-Poppins font-semibold hover:text-purple text-sm md:text-[20px] py-2 mb-[40px] md:mx-4",
   },
   {
     title: "Mon Compte",
-    path: "update-profile",
+    path: "/update-profile",
     cName:
       "block text-white font-Poppins font-semibold hover:text-purple text-sm md:text-[20px] py-2 mb-[40px] md:mx-4",
   },
