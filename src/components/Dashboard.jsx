0274e9537201fbff216873209ddacb0b8fe4f2ac
Sma-source <rabhi.smain@gmail.com>
2022-01-23 19:02:11 +0100

    update: layout

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import LayoutDash from "./LayoutDash";
 import QrCard from "./QrCard";
+import QrCardBlur from "./QrCardBlur";
 
 const Dashboard = () => {
+  const qrCode = false;
   return (
     <LayoutDash>
       <QrCard />
+      <QrCard />
+      {!qrCode && <QrCardBlur />}
     </LayoutDash>
   );
 };
