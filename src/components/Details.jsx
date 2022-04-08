394376f66c788d8e941b457068ecfcea93f08a08
Sma-source <rabhi.smain@gmail.com>
2022-04-08 22:47:16 +0200

    render firebase realtime data

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,42 +1,75 @@
-import { doc } from "firebase/firestore";
-import React from "react";
+import { doc, onSnapshot } from "firebase/firestore";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { useParams } from "react-router-dom";
-import { useEffect } from "react/cjs/react.production.min";
 import { db } from "../firebase";
 import LayoutDash from "./LayoutDash";
+import QRCode from "qrcode.react";
 
 function Details() {
+  const [code, setCode] = useState([]);
   const { id } = useParams();
+
+  useEffect(() => {
+    const unsub = onSnapshot(doc(db, "testqr", id), (doc) => {
+      let list = [];
+      list.push(doc.data());
+
+      setCode(list);
+      // console.log(code);
+    });
+    return () => {
+      unsub();
+    };
+  }, []);
+
   return (
     <LayoutDash>
       <div className="container min-h-screen mx-auto my-16 p-10 bg-black xs:w-full">
         <div className="container mx-auto">
-          <div className="flex flex-col md:flex-row items-center md:justify-center">
-            <img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1024px-QR_code_for_mobile_English_Wikipedia.svg.png"
-              alt="qr-code"
-              className=" w-[207px] md:w-[267px] bg-white"
-            />
-            <div className="flex flex-col md:w-1/2 lg:w-1/3 2xl:w-1/4 mx-4 md:p-5  text-center">
-              <p className="text-white font-bold font-Poppins  md:text-5xl  leading-relaxed mb-0">
-                50
-              </p>
-              <h3
-                className="font-normal
+          {code?.map((bou, i) => {
+            return (
+              <div
+                key={i}
+                className="flex flex-col md:flex-row items-center md:justify-center"
+              >
+                {/* <img
+                      src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1024px-QR_code_for_mobile_English_Wikipedia.svg.png"
+                      alt="qr-code"
+                      className=" w-[207px] md:w-[267px] bg-white"
+                    /> */}
+                <QRCode
+                  style={{ padding: 6 + "px" }}
+                  className="qrcode w-[117px] md:w-[267px] bg-white"
+                  size={bou.size}
+                  value={bou.value}
+                  bgColor="white"
+                  fgColor="#000"
+                  level="L"
+                  renderAs="svg"
+                ></QRCode>
+
+                <div className="flex flex-col md:w-1/2 lg:w-1/3 2xl:w-1/4 mx-4 md:p-5  text-center">
+                  <p className="text-white font-bold font-Poppins  md:text-5xl  leading-relaxed mb-0">
+                    50
+                  </p>
+                  <h3
+                    className="font-normal
                         font-Poppins
                         text-purple
                         text-xs
                         md:text-[16px]
                         mb-6 
                         "
-              >
-                scans
-              </h3>
-              <h3>
-                <a
-                  href="#"
-                  className="
+                  >
+                    scans
+                  </h3>
+                  <h3>
+                    <a
+                      target="_blank"
+                      rel="noreferrer"
+                      href={`https://${bou.value}`}
+                      className="
                         font-normal
                         font-Poppins
                         text-purple
@@ -47,14 +80,14 @@ function Details() {
                         mb-4
                         block
                         "
-                >
-                  https://example.com
-                </a>
-              </h3>
+                    >
+                      https://{bou.value}
+                    </a>
+                  </h3>
 
-              <Link
-                to="/details"
-                className="
+                  <Link
+                    to="/details"
+                    className="
                      
                      mb-3
                      md:w-full
@@ -70,12 +103,12 @@ function Details() {
                      font-semibold
                      transition
                      "
-              >
-                Modifier
-              </Link>
-              <a
-                href="#"
-                className="
+                  >
+                    Modifier
+                  </Link>
+                  <a
+                    href="#"
+                    className="
                      
                      md:w-full
                      
@@ -93,11 +126,13 @@ function Details() {
                       md:mb-0
                      
                      "
-              >
-                Télécharger
-              </a>
-            </div>
-          </div>
+                  >
+                    Télécharger
+                  </a>
+                </div>
+              </div>
+            );
+          })}
           <div className="flex flex-col p-10">
             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
