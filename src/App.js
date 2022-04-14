4d6ee9cdba46d48803893d31d91e73969fa50aed
Sma-source <rabhi.smain@gmail.com>
2022-04-14 16:38:06 +0200

    change path

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
             }
           />
           <Route
-            path="/:id"
+            path="/details/:id"
             element={
               <ProtectedRoute>
                 <Details />
