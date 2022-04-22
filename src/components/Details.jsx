f8070c1fad7231d61024d33efb10ffcee96a667a
Sma-source <rabhi.smain@gmail.com>
2022-04-22 12:06:21 +0200

    add delete method

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,28 +1,61 @@
-import { doc, onSnapshot } from "firebase/firestore";
+import { deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import React, { useEffect, useState, useRef } from "react";
-import { Link } from "react-router-dom";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { db } from "../firebase";
 import LayoutDash from "./LayoutDash";
+
 import QRCode from "qrcode.react";
 
 function Details() {
   const [code, setCode] = useState([]);
+  const [open, setOpen] = useState(false);
   const { id } = useParams();
   const qrcodeRef = useRef();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    const unsub = onSnapshot(doc(db, "testqr", id), (doc) => {
+    getQr();
+  }, []);
+
+  const getQr = () => {
+    const docRef = doc(db, "testqr", id);
+    onSnapshot(docRef, (doc) => {
       let list = [];
-      list.push(doc.data());
+      // list.push(doc.data());
+      const qr = {
+        id: doc.id,
+        ...doc.data(),
+      };
+
+      list.push(qr);
 
       setCode(list);
-      // console.log(code);
     });
-    return () => {
-      unsub();
-    };
-  }, []);
+  };
+
+  const handlePop = () => {
+    setOpen(!open);
+  };
+
+  // const readQrById = async (id) => {
+  //   const doc = await Firestore.collection("testqr").doc(id).get();
+
+  //   const qr = {
+  //     id: doc.id,
+  //     ...doc.data(),
+  //   };
+
+  //   console.log(qr);
+
+  //   return qr;
+  // };
+
+  const deleteQrcode = async (id) => {
+    const docRef = doc(db, "testqr", id);
+    await deleteDoc(docRef);
+    navigate("/");
+  };
 
   const downloadQRCode = (qrRef) => (e) => {
     e.preventDefault();
@@ -51,115 +84,121 @@ function Details() {
     <LayoutDash>
       <div className="container min-h-screen mx-auto my-16 p-10 bg-black xs:w-full">
         <div className="container mx-auto">
-          {code?.map((bou, i) => {
-            return (
-              <div
-                key={i}
-                className="flex flex-col md:flex-row items-center md:justify-center"
-              >
-                {/* <img
+          {code.length
+            ? code.map((data) => {
+                return (
+                  <div
+                    key={data.id}
+                    className="flex flex-col md:flex-row items-center md:justify-center"
+                  >
+                    {/* <img
                       src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1024px-QR_code_for_mobile_English_Wikipedia.svg.png"
                       alt="qr-code"
                       className=" w-[207px] md:w-[267px] bg-white"
                     /> */}
-                <li ref={qrcodeRef}>
-                  <QRCode
-                    style={{ padding: 6 + "px" }}
-                    className="qrcode w-[117px] md:w-[267px] bg-white"
-                    size={bou.size}
-                    value={bou.value}
-                    bgColor="white"
-                    fgColor="#000"
-                    level="L"
-                    renderAs="svg"
-                  ></QRCode>
-                </li>
+                    <li ref={qrcodeRef}>
+                      <QRCode
+                        style={{ padding: 6 + "px" }}
+                        className="qrcode w-[117px] md:w-[267px] bg-white"
+                        size={data.size || 250}
+                        value={data.value || "default"}
+                        bgColor="white"
+                        fgColor="#000"
+                        level="L"
+                        renderAs="svg"
+                      ></QRCode>
+                    </li>
 
-                <div className="flex flex-col md:w-1/2 lg:w-1/3 2xl:w-1/4 mx-4 md:p-5  text-center">
-                  <p className="text-white font-bold font-Poppins  md:text-5xl  leading-relaxed mb-0">
-                    50
-                  </p>
-                  <h3
-                    className="font-normal
-                        font-Poppins
-                        text-purple
-                        text-xs
-                        md:text-[16px]
-                        mb-6 
-                        "
-                  >
-                    scans
-                  </h3>
-                  <h3>
-                    <a
-                      target="_blank"
-                      rel="noreferrer"
-                      href={`https://${bou.value}`}
-                      className="
-                        font-normal
-                        font-Poppins
-                        text-purple
-                        text-xs
-                        md:text-[16px]
-                        mt-1
-                        md:mt-0
-                        mb-4
-                        block
-                        "
-                    >
-                      https://{bou.value}
-                    </a>
-                  </h3>
+                    <div className="flex flex-col md:w-1/2 lg:w-1/3 2xl:w-1/4 mx-4 md:p-5  text-center">
+                      <p className="text-white font-bold font-Poppins  md:text-5xl  leading-relaxed mb-0">
+                        50
+                      </p>
+                      <h3
+                        className="font-normal
+                         font-Poppins
+                         text-purple
+                         text-xs
+                         md:text-[16px]
+                         mb-6 
+                         "
+                      >
+                        scans
+                      </h3>
+                      <h3>
+                        <a
+                          target="_blank"
+                          rel="noreferrer"
+                          href={`https://${data.value}`}
+                          className="
+                         font-normal
+                         font-Poppins
+                         text-purple
+                         text-xs
+                         md:text-[16px]
+                         mt-1
+                         md:mt-0
+                         mb-4
+                         block
+                         "
+                        >
+                          https://{data.value}
+                        </a>
+                      </h3>
 
-                  <Link
-                    to="/details"
-                    className="
-                     
-                     mb-3
-                     md:w-full
-                     md:py-3
-                     md:px-10
-                     bg-gris
-                     border border-[#707070]
-                     rounded-full
-                     text-white
+                      <button
+                        disabled={loading}
+                        onClick={() => {
+                          deleteQrcode(data.id);
+                        }}
+                        className="
+                      
+                      mb-3
+                      md:w-full
+                      md:py-3
+                      md:px-10
+                      bg-gris
+                      border border-[#707070]
+                      rounded-full
+                      text-white
+                       text-sm
+                      md:text-lg
+                      font-Poppins
+                      font-semibold
+                      transition
+                      "
+                      >
+                        Delete
+                      </button>
+                      <button
+                        onClick={downloadQRCode(qrcodeRef)}
+                        type="submit"
+                        className="
+                      
+                      md:w-full
+                      
+                      md:py-3
+                      md:px-12
+                      bg-purple
+                      border border-[#707070]
+                      rounded-full
+                      text-white
                       text-sm
-                     md:text-lg
-                     font-Poppins
-                     font-semibold
-                     transition
-                     "
-                  >
-                    Modifier
-                  </Link>
-                  <button
-                    onClick={downloadQRCode(qrcodeRef)}
-                    type="submit"
-                    className="
-                     
-                     md:w-full
-                     
-                     md:py-3
-                     md:px-12
-                     bg-purple
-                     border border-[#707070]
-                     rounded-full
-                     text-white
-                     text-sm
-                     md:text-lg
-                     font-Poppins
-                     font-semibold
-                      mb-2
-                      md:mb-0
-                     
-                     "
-                  >
-                    Télécharger
-                  </button>
-                </div>
-              </div>
-            );
-          })}
+                      md:text-lg
+                      font-Poppins
+                      font-semibold
+                       mb-2
+                       md:mb-0
+                      
+                      "
+                      >
+                        Télécharger
+                      </button>
+                    </div>
+                  </div>
+                );
+              })
+            : ""}
+
           <div className="flex flex-col p-10">
             <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
               <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
